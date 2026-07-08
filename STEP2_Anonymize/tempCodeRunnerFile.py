dbf97593dd01d9b26c824f66db1b527fc56a432d
Le Nhat Tung <lenhattung@gmail.com>
2026-07-08 11:12:54 +0700

    u
    u

--- STEP2_Anonymize/tempCodeRunnerFile.py
+++ STEP2_Anonymize/tempCodeRunnerFile.py
@@ -1,23 +1,13 @@
-if __name__ == "__main__":
-#     import argparse
+print(anonymize_pipeline("Nguyễn Tuấn Kiệt trường hok có đọc đâu bà ơi 🥲🥲🥲"))
 
-#     parser = argparse.ArgumentParser(description="Ẩn danh tên người trong content (CRF - underthesea)")
-#     parser.add_argument("--input", required=True, help="Đường dẫn file input (csv/xlsx)")
-#     parser.add_argument("--output", required=True, help="Đường dẫn file output (csv/xlsx)")
-#     parser.add_argument("--content_col", default="content", help="Tên cột chứa nội dung")
-#     parser.add_argument("--min_len", type=int, default=20, help="Độ dài tối thiểu để giữ lại")
-#     parser.add_argument("--audit_size", type=int, default=200, help="Số dòng mẫu để audit thủ công")
-#     parser.add_argument(
-#         "--no_extend", action="store_true",
-#         help="Tắt fallback mở rộng sang token kế tiếp (ưu tiên Precision, giảm Recall)"
-#     )
-#     args = parser.parse_args()
+print(anonymize_pipeline("Hùng Minh môn hiểu biết về DNTU:)))???"))
 
-#     run(
-#         input_path=args.input,
-#         output_path=args.output,
-#         content_col=args.content_col,
-#         min_len=args.min_len,
-#         audit_sample_size=args.audit_size,
-#         extend_to_next_token=not args.no_extend,
-#     )
+print(anonymize_pipeline("Hoàng Nam bào nào mục xương thì thôi"))
+
+print(anonymize_pipeline("Các bạn có biết thầy Lê Nhật Tùng không?"))
+
+print(anonymize_pipeline("tui không biết cô nguyễn thị liệu"))
+
+print(anonymize_pipeline("sinh viên phạm thế an là ai?"))
+
+print(anonymize_pipeline("thông báo về vi phạm nguyên tắc cộng đồng"))
